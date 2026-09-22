55552728f03dbe1ada02047151909f6442e63863
Arturo López Herrera <lopezherreraarturojashiel@gmail.com>
2026-09-22 08:48:08 -0600

    Versión_02_1: Propiedades_MiArreglo

--- Arreglos.Logica/MiArreglo.cs
+++ Arreglos.Logica/MiArreglo.cs
@@ -6,5 +6,23 @@ namespace Arreglos.Logica
 {
     public class MiArreglo
     {
+        //Atributos o campos
+        private int _tope;
+        private int[] _arreglo;
+
+        //Constructor
+        public MiArreglo(int n)
+        {
+            N = n;
+            _arreglo = new int[N];
+            _tope = 0;
+        }
+
+        //Propiedades
+        public int N { get; }
+        public bool EstaVacio => _tope == 0;
+        public bool EstaLleno => _tope == N;
+
+        //Métodos
     }
 }
